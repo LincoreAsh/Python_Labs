31cfe5b7640e05cf33079c22f9ac5cc32bbadf50
LincoreAsh <66129940+LincoreAsh@users.noreply.github.com>
2022-03-03 17:29:37 +0400

    Create Lab2.py

---
+++ Lab#2/Lab2.py
@@ -0,0 +1,21 @@
+with open("input.txt") as file:
+  n = file.read().split()
+  n = list(map(int, n))
+  print(*n)
+l, ind = len(n), 0
+
+for x in range(len(n))[::-1]:
+  index = n.index(n[x])
+  if not (not n[x] % 2 and x%2):
+    del(n[x])
+
+print("Полученная последовательность чисел =", *n)
+
+for value in n:
+    s = 0
+    v = value
+    while v:
+        s += 1
+        v //= 10
+    print(value, "- Номер символа в последовательности, с которого начинается число =", ind, "- Количество цифр в числе =", s)
+    ind = ind+s+1
