812f14b6a5661bafc7b1d787247c4ac751b8b1c5
LincoreAsh <66129940+LincoreAsh@users.noreply.github.com>
2022-03-18 22:59:42 +0400

    Update Lab2.py

--- Lab#2/Lab2.py
+++ Lab#2/Lab2.py
@@ -1,20 +1,33 @@
-with open("input.txt") as file:
-  n = file.read().split()
-  n = list(map(int, n))
-  print(*n)
-l, ind = len(n), 0
-
-for x in range(len(n))[::-1]:
-  if not (not n[x] % 2 and x%2):
-    del(n[x])
-
-print("Полученная последовательность чисел =", *n)
-
-for value in n:
-    s = 0
+"""Написать программу, которая читая последовательность чисел из файла, выводит на экран четные числа на нечетных
+местах. После каждого числа выводится, номер символа в последовательности (цифры, а не числа), с которого начинается
+число и количество цифр в нем. """
+import time, json, random
+from random import randint
+start = time.time()
+rnd_length = [10, 100, 1000, 10000, 100000]  # Массив для случайной длины числа
+text = [randint(1, random.choice(rnd_length)) for i in range(1000)]  # Заполнение массива случайными числами
+with open('input.txt', 'w') as fw:  # Запись случайных чисел в файл, если его нету, он создаётся
+    json.dump(text, fw)
+with open("input.txt", 'r') as fr:  # Читаем файл
+    text = json.load(fr)
+print("Исходная последовательность:", *text, "\nПолученная последовательность: ")
+count, index_count, digits_num, index, posledov = 0, 0, [], [], []
+for value in text:
+    sum = 0
     v = value
-    while v:
-        s += 1
+    posledov.append(v)
+    while v:  # Подсчет количества цифр в числе
+        sum += 1
         v //= 10
-    print(value, "- Номер символа в последовательности, с которого начинается число =", ind, "- Количество цифр в числе =", s)
-    ind = ind+s+1
+    digits_num.append(sum)
+    index.append(index_count)  # Подсчет номера символа, с которого начинается число
+    index_count = index_count + sum + 1
+for x in range(len(text))[::-1]:  # Удаление нечетных чисел на четных местах
+    if not (not text[x] % 2 and x % 2):
+        del (text[x], posledov[x], digits_num[x], index[x])
+for x in text:
+    print("Число =", posledov[count], "; Номер символа в последовательности, с которого начинается число =",
+          index[count],
+          "; Количество цифр в числе =", digits_num[count])
+    count += 1
+print("Program time: {:>.10f}".format(time.time() - start) + " seconds.")
